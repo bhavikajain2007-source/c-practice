87c85367baf70a3c6d3aa52f9a34d8a3fcd7def5
bhavikajain2007-source <bhavikajain2007@gmail.com>
2026-02-21 17:08:50 +0530

    Add files via upload

---
+++ practice day 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter x:";
+    cin>>x;
+
+    if(x>0){
+        cout<<"positive";
+    }
+    else{
+        cout<<"negative";
+    
+    }
+
+}
